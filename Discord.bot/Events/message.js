ba2a6e6ea6dc78e6d96c14347d2c0259933dbfc4
Mathieu DEGUEURCE <degueurce.mathieu@gmail.com>
2020-03-06 18:55:01 +0100

    mise en place d'un fichier avec toute les variable et import

---
+++ Discord.bot/Events/message.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const prefix = "%"; 
+
+module.exports = async(client, message) => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    if (message.content.startWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const commande = args.shift();
+    const cmd = client.commands.get(commande);
+    if (!cmd) return; 
+    cmd.run(client, message, args); 
+};
